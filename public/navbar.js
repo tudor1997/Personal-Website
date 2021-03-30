bc618d5ee5e8db1ee58a749faefb795e0421db9c
DESKTOP-OP0FSSP\anast <anastasiu.tudor@gmail.com>
2021-03-30 17:00:47 +0300

    css bugs fixed

--- public/navbar.js
+++ public/navbar.js
@@ -1,11 +1,11 @@
 const navbar = document.querySelector('.navbar');
 const hamburger = document.querySelector('.hamburger');
 const closeMenu = document.querySelector('.closeMenu');
-
+const navElems = document.querySelectorAll('.nav-item');
 
 hamburger.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
-
+navElems.forEach(elem => elem.addEventListener('click', close));
 function show(){
     navbar.style.display = "flex";
     navbar.style.top = "0";
